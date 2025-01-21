0c67eda5c464068f246f62cafd93a517c8bbc73d
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-01-21 16:27:46 -0300

    feat: Estrutura da aplicação - Componente: Post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,45 @@
-export function Post({author, content}) {
+import styles from "./Post.module.css";
+
+export function Post() {
   return (
-    <div>
-      <strong>{author}</strong>
-      <p>{content}</p>
-    </div>
-  )
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/CaioDetore.png"
+            alt=""
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Caio Detore</strong>
+            <span>UX Designer</span>
+          </div>
+
+          <time title="21 de Janeiro de 2025" dateTime="2025-01-21 15:00:00">
+            Publicado há 1h
+          </time>
+        </div>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          👉 <a href=""> jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto</a>{" "}
+          <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
 }
