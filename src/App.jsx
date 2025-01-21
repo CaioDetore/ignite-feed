67e79de197901f6454db118ff751f0848da15bd6
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-01-21 15:24:04 -0300

    feat: Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { Header } from './components/Header/Header'
+
+import styles from './App.module.css'
+
+import { Post } from './components/Post/Post'
+import { Sidebar } from './components/Sidebar/Sidebar'
+
 import './styles.css'
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="aaaaaa"
+            content="bbbbbbbbbbb"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from "@phosphor-icons/react";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1698273300841-d9aee5c58922?q=80&w=985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/CaioDetore.png" />
+
+        <strong>Caio Detore</strong>
+        <span>UX Designer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
