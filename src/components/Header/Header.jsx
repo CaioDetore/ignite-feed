0c243fa8315b20980dbff1c85c0df091fbb8f88e
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-01-21 14:08:51 -0300

    feat: Estrutura da aplicacao - Componente: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import igniteLogo from '../../assets/logo.svg'
+
 export function Header() {
   return (
-    <div className={styles.header}>
-      <strong>Ignite Feed</strong>
-    </div>
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logo Ignite Feed" />
+    </header>
   );
 }
