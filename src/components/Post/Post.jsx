65708f2fe675b8044571529b991361e31e3a91d4
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-01-22 15:01:05 -0300

    feat: componente de avatar

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "../Avatar/Avatar";
 import { Comment } from "../Comment/Comment";
 import styles from "./Post.module.css";
 
@@ -6,11 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/CaioDetore.png"
-            alt=""
-          />
+          <Avatar src="https://github.com/CaioDetore.png" alt="" />
 
           <div className={styles.authorInfo}>
             <strong>Caio Detore</strong>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "@phosphor-icons/react";
 
 import styles from "./Sidebar.module.css";
+import { Avatar } from "../Avatar/Avatar";
 
 export function Sidebar() {
   return (
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img src="https://github.com/CaioDetore.png" />
+        <Avatar src="https://github.com/CaioDetore.png" />
 
         <strong>Caio Detore</strong>
         <span>UX Designer</span>
