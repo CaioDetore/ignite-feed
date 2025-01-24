e60c71327a7427a5cbe85c0c9bded4e3b431c7a3
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-01-23 21:11:04 -0300

    feat: aplaudir comentario

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -3,10 +3,17 @@
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import styles from "./Comment.module.css";
 import { Avatar } from "../Avatar/Avatar";
+import { useState } from "react";
 
 export function Comment({content, onDeleteComment}) {
+  const [likeCount, setLikeCount] = useState(0)
+  
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(state => state + 1)
   }
 
   return (
@@ -36,9 +43,9 @@ export function Comment({content, onDeleteComment}) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20 </span>
+            Aplaudir <span>{likeCount} </span>
           </button>
         </footer>
       </div>
